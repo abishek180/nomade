9ad9f9822afa12bed041ed1a4d36623d89e3ba15
abishek180 <anilabishek15@gmail.com>
2024-01-10 17:45:32 +0530

    changes in footer

--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,17 @@
 import React from "react";
-import logo from "/public/Images/Rectangle.png"
+import logo from "/public/Images/Rectangle.png";
 import Image from "next/image";
 
 const footer = () => {
   return (
     <footer>
       <div className="h-[661px] bg-[#070C29] relative">
-       <div className="pt-80">
-       <div className="w-[80px] h-[46px] lg:w-[112px] lg:h-[46px] flex  justify-center items-center">
-          
-          <Image src={logo} className="w-[100%]" alt="logo" />
-          <p>sugfudshvdfgvfvfdavdfv xcvxcvx</p>
-          <p>okdjvgfdbdfbjdfabdfbdfb</p>
-        
-      </div>
-       </div>
+        <div className="pt-80 max-w-screen-xl mx-auto px-[16px]">
+          <div className="w-[80px] h-[46px] lg:w-[112px] lg:h-[46px]">
+            <Image src={logo} className="w-[100%]" alt="logo" />
+            <p className="text-[#8D90A8] text-[16px] font-medium leading-[28px]">#7th Street, abc , Mauritania North West Africa 425680 </p>
+          </div>
+        </div>
       </div>
     </footer>
   );
